54ca530ae5f2e7b9cc9441ffcc5c4114a027d1f3
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-05-02 11:07:54 -0300

    Switch Case

---
+++ src/main/java/Exercicios_Gradle_Java/ExercicioSwitchCase.java
@@ -0,0 +1,36 @@
+
+package Exercicios_Gradle_Java;
+
+public class ExercicioSwitchCase {
+    
+    public static void main(String[] args) {
+        
+        byte dia = 4;
+        
+        switch(dia){
+            case 1:
+                System.out.println("Domingo - Final de semana");
+                break;
+            case 2:
+                System.out.println("Segunda - Dia util");
+                break;
+            case 3:
+                System.out.println("Terca - Dia util");
+                break;
+            case 4:
+                System.out.println("Quarta - Dia util");
+                break;
+            case 5:
+                System.out.println("Quinta - Dia util");
+                break;
+            case 6:
+                System.out.println("Sexta - Dia util");
+                break;
+            case 7:
+                System.out.println("Sabado - Final de semana");
+                break;
+                
+        }
+    }
+    
+}
---
+++ src/main/java/Exercicios_Gradle_Java/SwitchCase.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Exercicios_Gradle_Java;
+
+/**
+ *
+ * @author lucas
+ */
+public class SwitchCase {
+    public static void main(String[] args) {
+        
+        byte dia = 10;
+        
+        switch(dia){
+            case 1:
+                System.out.println("domingo");
+                break;
+            case 2:
+                System.out.println("segunda");
+                break;
+            case 3:
+                System.out.println("terça");
+                break;
+            case 4:
+                System.out.println("quarta");
+                break;
+            case 5:
+                System.out.println("quinta");
+                break;
+            case 6:
+                System.out.println("sexta");
+                break;
+            case 7:
+                System.out.println("sabado");
+                break;
+            default:
+                System.out.println("Opcao invalida");
+                break;
+        }
+    }
+}
