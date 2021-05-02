96984349ce960921cd90863ab409c008f3d36491
Lucas Souza <79222458+lucasdev3@users.noreply.github.com>
2021-05-02 10:38:49 -0300

    Att2

--- src/main/java/Exercicios_Gradle_Java/ControleDeFluxo.java
+++ src/main/java/Exercicios_Gradle_Java/ControleDeFluxo.java
@@ -7,7 +7,7 @@ package Exercicios_Gradle_Java;
  */
 public class ControleDeFluxo {
     public static void main(String[] args) {
-        double salario = 1500d;
+        double salario = 15000d;
         double imposto;
         
         
@@ -19,17 +19,17 @@ public class ControleDeFluxo {
         else if (salario >= 1000 && salario < 2000){
             imposto = (salario * 10 / 100);
             System.out.println("10% de imposto");
-            System.out.println("Salario com reajuste: R$"+ imposto);
+            System.out.println("Imposto: R$"+ imposto);
         }
         else if (salario >= 2000 && salario < 4000){
             imposto = (salario * 15 / 100);
             System.out.println("15% de imposto");
-            System.out.println("Salario com reajuste: R$"+ imposto);
+            System.out.println("Imposto: R$"+ imposto);
         }
         else{
             imposto = (salario * 20 / 100);
             System.out.println("20% de imposto");
-            System.out.println("Salario com reajuste: R$"+ imposto);
+            System.out.println("Imposto: R$"+ imposto);
         }
 }
 
